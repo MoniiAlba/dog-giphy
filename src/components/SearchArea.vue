11728f7a6da19e854a5fd5b4c8850c401e205c99
Monica Alba <soeur_azmaria@hotmail.com>
2020-05-09 21:25:12 -0500

    search bar before funcionality

---
+++ src/components/SearchArea.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="search-container">
+        <div class="search-bar">
+            <v-text-field
+                v-model="searchGif"
+                outlined
+                autofocus
+                loading="showLoading"
+                append-icon="fas fa-search"
+                @keyup.enter="search"
+                >
+            </v-text-field>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            showLoading: false,
+            searchGif: 'Dog'
+        } 
+    },
+    methods: {
+        search() {
+            this.showLoading = true
+            console.log(this.searchGif)
+            
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+  .search-container{
+      margin: 40px;      
+  }
+
+  .search-bar{
+      width: 25%;
+  }
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="navbar-container">
+        <div class="navbar">
+            <v-icon color="white">fas fa-paw</v-icon>
+            <div class="navbar-title">
+                Dog Finder
+            </div>
+            <v-icon color="white">fas fa-paw</v-icon>
+        </div>
+    </div>
+</template>
+
+<style  scoped>
+.navbar{
+    height: 65px;
+    width: 100%;
+    background-color: #159dc6;
+    color: white;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    
+}
+
+.navbar-title{
+    font-style: oblique;
+    font-family: monospace;
+    font-size: 25px;
+}
+
+</style>
